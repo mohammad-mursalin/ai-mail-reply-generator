0b5b7b2082e0cfe45c7a80152ac8c89bef95ed61
mohammad-mursalin <mursalinlamon@gmail.com>
2025-02-16 00:13:46 +0600

    initial commit

---
+++ src/main/java/com/mursalin/ai_mail_reply/AiMailReplyApplication.java
@@ -0,0 +1,13 @@
+package com.mursalin.ai_mail_reply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMailReplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiMailReplyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mursalin/ai_mail_reply/AiMailReplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mursalin.ai_mail_reply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiMailReplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
