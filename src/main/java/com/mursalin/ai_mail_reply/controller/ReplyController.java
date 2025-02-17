e39378c5336aa3ff31a44185c7e829d96a4232e0
mohammad-mursalin <mursalinlamon@gmail.com>
2025-02-17 22:50:51 +0600

    controller class

---
+++ src/main/java/com/mursalin/ai_mail_reply/controller/ReplyController.java
@@ -0,0 +1,9 @@
+package com.mursalin.ai_mail_reply.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ai-reply")
+public class ReplyController {
+}
