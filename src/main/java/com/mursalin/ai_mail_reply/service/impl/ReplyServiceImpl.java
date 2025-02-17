d67a08cf8fe9d7ca0b454f242fd0c1f077691e0a
mohammad-mursalin <mursalinlamon@gmail.com>
2025-02-17 22:59:45 +0600

    required variables and methods

--- src/main/java/com/mursalin/ai_mail_reply/service/impl/ReplyServiceImpl.java
+++ src/main/java/com/mursalin/ai_mail_reply/service/impl/ReplyServiceImpl.java
@@ -1,4 +1,25 @@
 package com.mursalin.ai_mail_reply.service.impl;
 
-public class ReplyServiceImpl {
+import com.mursalin.ai_mail_reply.service.ReplyService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class ReplyServiceImpl implements ReplyService {
+
+    @Value("${gemini.api.key}")
+    private String gemini_api_key;
+
+    @Value("${gemini.api.url}")
+    private String gemini_api_url;
+
+    private final WebClient webClient;
+
+    public ReplyServiceImpl(WebClient.Builder webClient) {
+        this.webClient = webClient.build();
+    }
+
+    @Override
+    public String generateReply(String mail) {
+        return "";
+    }
 }
