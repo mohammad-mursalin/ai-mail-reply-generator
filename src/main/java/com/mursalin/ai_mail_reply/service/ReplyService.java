398ef2975d2b8dcbecc708100dfd6ac6a0764b11
mohammad-mursalin <mursalinlamon@gmail.com>
2025-02-18 00:06:43 +0600

    dto class used

--- src/main/java/com/mursalin/ai_mail_reply/service/ReplyService.java
+++ src/main/java/com/mursalin/ai_mail_reply/service/ReplyService.java
@@ -1,4 +1,7 @@
 package com.mursalin.ai_mail_reply.service;
 
+import com.mursalin.ai_mail_reply.dto.MailRequest;
+
 public interface ReplyService {
+    String generateReply(MailRequest mailRequest);
 }
