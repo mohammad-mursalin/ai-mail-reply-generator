1a042b644644964d986cbc934d54783599598510
mohammad-mursalin <mursalinlamon@gmail.com>
2025-02-18 00:06:35 +0600

    dto class used

--- src/main/java/com/mursalin/ai_mail_reply/controller/ReplyController.java
+++ src/main/java/com/mursalin/ai_mail_reply/controller/ReplyController.java
@@ -1,9 +1,22 @@
 package com.mursalin.ai_mail_reply.controller;
 
+import com.mursalin.ai_mail_reply.dto.MailRequest;
+import com.mursalin.ai_mail_reply.service.ReplyService;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/ai-reply")
 public class ReplyController {
+
+    private final ReplyService replyService;
+
+    public ReplyController(ReplyService replyService) {
+        this.replyService = replyService;
+    }
+
+    public String generateReply(@RequestBody MailRequest mailRequest) {
+        return replyService.generateReply(mailRequest);
+    }
 }
